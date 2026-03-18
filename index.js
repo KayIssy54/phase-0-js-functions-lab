3901b4a34a54c4f50ad953ce42951986d86c8258
KayIssy54 <mwongerajoy4@gmail.com>
2026-03-18 21:33:08 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 
+function calculateTax(amount) {
+    return amount * 0.10; // 10% tax
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+
+function isPalindrome(word) {
+    const cleanedWord = word.toLowerCase();
+    return cleanedWord === cleanedWord.split('').reverse().join('');
+}
+
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * (discountPercentage / 100));
+}
 
 
 
