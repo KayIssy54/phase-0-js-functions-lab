5b23ed758b7b6fbe3039039d18c8de9fad67a696
KayIssy54 <mwongerajoy4@gmail.com>
2026-04-16 14:01:13 +0300

    Fix all functions and pass full test suite

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-
 function calculateTax(amount) {
-    return amount * 0.10; // 10% tax
+    return amount * 0.10;
 }
 
 function convertToUpperCase(text) {
     return text.toUpperCase();
 }
 
-
 function findMaximum(num1, num2) {
     return num1 > num2 ? num1 : num2;
 }
-
 
 function isPalindrome(word) {
     const cleanedWord = word.toLowerCase();
     return cleanedWord === cleanedWord.split('').reverse().join('');
 }
 
-
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
     return originalPrice - (originalPrice * (discountPercentage / 100));
 }
 
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// required export for tests
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+};
